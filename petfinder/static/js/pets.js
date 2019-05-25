594db4deb296a8f16b56da5f0bd9598a5803bf24
Sumit Dugar <dugar.sumit1990@gmail.com>
2019-05-25 20:22:09 +0200

    fixed detail modal bug

--- petfinder/static/js/pets.js
+++ petfinder/static/js/pets.js
@@ -179,7 +179,7 @@ function getCookie(name) {
 }
 
 /* Upload button */
-function uploadajax(ttl, cl) {
+function uploadajax(cl) {
     var fileList = $('#multiupload').prop("files");
     $('#prog' + cl).removeClass('loading-prep').addClass('upload-image');
 
@@ -204,11 +204,11 @@ function uploadajax(ttl, cl) {
             var xhr = $.ajaxSettings.xhr();
             if (xhr.upload) {
                 xhr.upload.addEventListener('progress', function (event) {
-                    var percent = 0;
-                    if (event.lengthComputable) {
-                        percent = Math.ceil(event.loaded / event.total * 100);
-                    }
-                    $('#prog' + cl).text(' ' + percent + '%')
+                    // var percent = 0;
+                    // if (event.lengthComputable) {
+                    //     percent = Math.ceil(event.loaded / event.total * 100);
+                    // }
+                    // $('#prog' + cl).text(' ' + percent + '%')
 
                 }, false);
             }
@@ -219,9 +219,6 @@ function uploadajax(ttl, cl) {
                 percent = 0;
                 $('#prog' + cl).text('');
                 $('#prog' + cl).text(' Success');
-                if (cl < ttl) {
-                    uploadajax(ttl, cl + 1);
-                }
             }
         },
         error: function (res) {
@@ -240,11 +237,8 @@ $('#upload-btn').click(function () {
     var i;
     for (i = 0; i < fileList.length; i++) {
         $('#upload-stats').append('<p class="upload-page">' + fileList[i].name + '<span class="loading-prep" id="prog' + i + '"></span></p>');
-        if (i == fileList.length - 1) {
-            uploadajax(fileList.length - 1, 0);
-        }
+        uploadajax(i);
     }
-
 });
 
 
