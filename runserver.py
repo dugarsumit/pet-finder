696591f379d6f7cb8276b6f0d6ca09063549bcce
Sumit Dugar <dugar.sumit1990@gmail.com>
2019-08-24 19:06:48 +0200

    heroko

--- runserver.py
+++ runserver.py
@@ -3,4 +3,5 @@ import subprocess
 
 print(os.environ['PORT'])
 host = "0.0.0.0" + ":" + os.environ['PORT']
+subprocess.call(["python", "petfinder/manage.py", "migrate"])
 subprocess.call(["python", "petfinder/manage.py", "runserver", host])
