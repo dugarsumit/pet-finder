ceabd39cdcefa72cb99a2a7fe3d73eb14ed103c8
Sumit Dugar <dugar.sumit1990@gmail.com>
2019-08-28 22:17:50 +0200

    Upload dir fix

--- petfinder/petfinder/settings.py
+++ petfinder/petfinder/settings.py
@@ -61,6 +61,9 @@ else:
 PET_PROFILE_IMG_DIR = '/img/pet-profile'
 
 UPLOAD_DIR = ROOT_PROJ_DIR + '/petfinder/static' + PET_PROFILE_IMG_DIR
+if not os.path.exists(UPLOAD_DIR):
+    os.makedirs(UPLOAD_DIR)
+
 
 ALLOWED_EXTENSION = ['jpg', 'jpeg', 'png']
 
